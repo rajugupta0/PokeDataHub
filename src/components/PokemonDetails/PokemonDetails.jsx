990c646ac64910e500a40cd311130f2eaa25d8d2
rajugupta0 <raju200115@gmail.com>
2024-02-13 17:36:38 +0530

    Added routes

--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,6 +1,13 @@
+import axios from "axios";
+import { useParams } from "react-router-dom"
 
 
 function PokemonDetails() {
+  const {id} =useParams();
+  async function downloadPokemons (){
+  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+  console.log(response.data);
+  }
   return (
     <>
     Details
