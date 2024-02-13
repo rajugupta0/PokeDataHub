af9b6ecab1b8969b3b82813edc62eca7a5a6b06a
rajugupta0 <raju200115@gmail.com>
2024-02-13 14:51:03 +0530

    V1 is Ready

--- src/components/Pokedex/Pokedex.jsx
+++ src/components/Pokedex/Pokedex.jsx
@@ -8,7 +8,7 @@ function Pokedex() {
 
     return (
         <div className="pokedex-wrapper">
-            <h1 id="pokedex-heading">Pokedex</h1>
+            <h1 id="pokedex-heading">PokedataHub</h1>
             <Search />
             <PokemonList />
         </div>
